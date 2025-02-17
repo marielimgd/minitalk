b6b3142003195a405640d89df969a61aeef1a364
Marieli Mariano Goncalves Dias <mmariano@c3r5p4.42sp.org.br>
2025-02-17 16:45:35 -0300

    fix bonus program

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 18:44:46 by mmariano          #+#    #+#             */
-/*   Updated: 2025/02/17 14:27:42 by mmariano         ###   ########.fr       */
+/*   Updated: 2025/02/17 16:38:15 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 void	send_signal(int pid, unsigned char c);
 void	process_signal(int signal);
-void	process_signal_back(int signal, siginfo_t *info);
+void	process_signal_back(int signal, siginfo_t *info, void *context);
 void	confirm_bits(int signal);
 
 #endif
--- src/client_bonus.c
+++ src/client_bonus.c
@@ -6,70 +6,65 @@
 /*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 19:54:02 by marieli           #+#    #+#             */
-/*   Updated: 2025/02/17 15:29:01 by mmariano         ###   ########.fr       */
+/*   Updated: 2025/02/17 16:37:47 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-void	confirm_bits(int signal)
-{
-	if (signal == SIGUSR1)
-		write(1, "Received bit 1\n", 15);
-	else if (signal == SIGUSR2)
-		write(1, "Received bit 0\n", 15);
+void confirm_bits(int signal) {
+    if (signal == SIGUSR1)
+        write(1, "Received bit 1\n", 15);
+    else if (signal == SIGUSR2)
+        write(1, "Received bit 0\n", 15);
 }
 
-void	send_signal(int pid, unsigned char c)
+void send_signal(int pid, unsigned char c) 
 {
-	int			bit;
-	sigset_t	mask;
+    int bit;
+    sigset_t mask, oldmask;
 
-	sigemptyset(&mask);
-	sigaddset(&mask, SIGUSR1);
-	sigaddset(&mask, SIGUSR2);
-	bit = 8;
-	while (bit > 0)
-	{
-		bit--;
-		if ((c >> bit) & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(100);
-		sigsuspend(&mask);
-	}
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+    bit = 8;
+    while (bit-- > 0) {
+        if ((c >> bit) & 1)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(100);
+        sigsuspend(&oldmask);
+    }
+    sigprocmask(SIG_SETMASK, &oldmask, NULL);
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int					pid;
-	const char			*message;
-	int					c;
-	struct sigaction	sa;
+    int pid;
+    const char *message;
+    int c;
 
-	if (argc != 3)
-	{
-		write(1, "Usage: ./client <pid> <message>\n", 33);
-		exit(1);
-	}
-	pid = ft_atoi(argv[1]);
-	message = argv[2];
-	c = 0;
-	sa.sa_handler = confirm_bits;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	while (message[c])
-	{
-		send_signal(pid, message[c]);
-		c++;
-	}
-	send_signal(pid, '\0');
-	return (0);
+    if (argc != 3)
+    {
+        write(1, "Usage: ./client_bonus <pid> <message>\n", 38);
+        exit(0);
+    }
+    pid = ft_atoi(argv[1]);
+    message = argv[2];
+    struct sigaction sa;
+    sa.sa_handler = confirm_bits;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    c = 0;
+    while (message[c])
+    {
+        send_signal(pid, message[c]);
+        c++;
+    }
+    send_signal(pid, '\0');
+    return (0);
 }
